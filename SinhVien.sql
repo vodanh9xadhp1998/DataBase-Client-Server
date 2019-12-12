4c35aec42bead2a1ff84127efe8707a75ef9c534
Linh Nguyễn <vodanh9xadhp1998@gmail.com>
2019-12-12 17:07:15 +0700

    first commit

---
+++ SinhVien.sql
@@ -0,0 +1,14 @@
+create database qlsv
+use qlsv
+create table dssv
+(
+	id int primary key,
+    ten nvarchar(50),
+    tuoi int,
+    mark float
+)
+insert into dssv value(1,'Linh',22,10)
+insert into dssv value(2,'Hoan',22,9)
+insert into dssv value(3,'Kien',22,8)
+insert into dssv value(4,'Hai',22,9.5)
+insert into dssv value(5,'Hoang',22,7)
